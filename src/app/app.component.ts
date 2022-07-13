f92a04aa09d666b3408dca0c5053fe110f76d3d7
Ana Paula <ana.guevara.valladares@gmail.com>
2022-07-13 17:59:02 -0500

    Updated: contador de 5 en 5 desde logica en componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'bases';
   contador:number = 10
+  base:number = 5
+
+  operation(type:number){
+    this.contador = (type === 1) ? (this.contador +1):(this.contador -1)
+  }
+
+  acumulador(type:number){
+    this.contador = (type === 1) ? (this.contador + this.base):(this.contador - this.base)
+  }
 }
